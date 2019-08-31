85b963a463c85657225be24c0497137739297ed5
India Hiring <indiahiring@global.publicisgroupe.net>
2019-08-31 14:46:47 +0530

    next commit

--- src/main/java/com/team/pos/TeamStandings/TeamStandingsApplication.java
+++ src/main/java/com/team/pos/TeamStandings/TeamStandingsApplication.java
@@ -1,20 +1,34 @@
 package com.team.pos.TeamStandings;
 
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 @EnableWebSecurity
@@ -24,7 +38,7 @@ public class TeamStandingsApplication {
 	RestAuthenticationEntryPoint restAuthenticationEntryPoint;
 	
 	@Autowired
-	AuthenticationSuccessHandler mySuccessHandler;
+	MySimpleUrlAuthenticationSuccessHandler mySuccessHandler;
 	
 	@Autowired
 	AuthenticationFailureHandler myFailureHandler;
@@ -51,6 +65,32 @@ public class TeamStandingsApplication {
 	    .logout();
 	}
 
+	@Component
+	public class CustomAuthenticationFailureHandler 
+	  implements AuthenticationFailureHandler {
+	  
+	    private ObjectMapper objectMapper = new ObjectMapper();
+	 
+	    @Override
+	    public void onAuthenticationFailure(
+	      HttpServletRequest request,
+	      HttpServletResponse response,
+	      AuthenticationException exception) 
+	      throws IOException, ServletException {
+	  
+	        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+	        Map<String, Object> data = new HashMap<>();
+	        data.put(
+	          "timestamp", 
+	          Calendar.getInstance().getTime());
+	        data.put(
+	          "exception", 
+	          exception.getMessage());
+	 
+	        response.getOutputStream()
+	          .println(objectMapper.writeValueAsString(data));
+	    }
+	}	
 	
 	@Component
 	public final class RestAuthenticationEntryPoint 
@@ -67,5 +107,72 @@ public class TeamStandingsApplication {
 	    }
 	}
 	
+	@Component
+	public class MySimpleUrlAuthenticationSuccessHandler
+	  implements AuthenticationSuccessHandler {
+	  
+	    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+	 
+	    @Override
+	    public void onAuthenticationSuccess(HttpServletRequest request, 
+	      HttpServletResponse response, Authentication authentication)
+	      throws IOException {
+	  
+	        handle(request, response, authentication);
+	        clearAuthenticationAttributes(request);
+	    }
+	    
+	    protected void handle(HttpServletRequest request, 
+	    	      HttpServletResponse response, Authentication authentication)
+	    	      throws IOException {
+	    	  
+	    	        String targetUrl = determineTargetUrl(authentication);
+	    	 
+	    	        if (response.isCommitted()) {
+	    	            return;
+	    	        }
+	    	 
+	    	        redirectStrategy.sendRedirect(request, response, targetUrl);
+	    	    }
+	    	 
+	    	    protected String determineTargetUrl(Authentication authentication) {
+	    	        boolean isUser = false;
+	    	        boolean isAdmin = false;
+	    	        Collection<? extends GrantedAuthority> authorities
+	    	         = authentication.getAuthorities();
+	    	        for (GrantedAuthority grantedAuthority : authorities) {
+	    	            if (grantedAuthority.getAuthority().equals("ROLE_USER")) {
+	    	                isUser = true;
+	    	                break;
+	    	            } else if (grantedAuthority.getAuthority().equals("ROLE_ADMIN")) {
+	    	                isAdmin = true;
+	    	                break;
+	    	            }
+	    	        }
+	    	 
+	    	        if (isUser) {
+	    	            return "/homepage.html";
+	    	        } else if (isAdmin) {
+	    	            return "/console.html";
+	    	        } else {
+	    	            throw new IllegalStateException();
+	    	        }
+	    	    }
+	    	 
+	    	    protected void clearAuthenticationAttributes(HttpServletRequest request) {
+	    	        HttpSession session = request.getSession(false);
+	    	        if (session == null) {
+	    	            return;
+	    	        }
+	    	        session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+	    	    }
+	    	 
+	    	    public void setRedirectStrategy(RedirectStrategy redirectStrategy) {
+	    	        this.redirectStrategy = redirectStrategy;
+	    	    }
+	    	    protected RedirectStrategy getRedirectStrategy() {
+	    	        return redirectStrategy;
+	    	    }
+	}
 	 
 }
--- src/main/java/com/team/pos/TeamStandings/controller/TeamController.java
+++ src/main/java/com/team/pos/TeamStandings/controller/TeamController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import com.team.pos.TeamStandings.mdel.TeamEntity;
 import com.team.pos.TeamStandings.repository.TeamRepository;
 
 @RestController
+@RequestMapping("/teams")
 public class TeamController {
 	
 	@Autowired
